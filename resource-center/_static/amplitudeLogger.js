42617546bbaa0fa335774949c09a14782e27df1b
brynn-udacity <brynn@udacity.com>
2015-12-12 12:26:32 -0800

    updates to international page and amplitude

--- resource-center/_static/amplitudeLogger.js
+++ resource-center/_static/amplitudeLogger.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-	var dev = true;
 	$('.toctree-l1').on('click', 'a', function(evt) {
 		var pageName = $(evt.target).attr('href');
 		pageName = 'career_' + pageName.replace(/(#|.html)/, '');
